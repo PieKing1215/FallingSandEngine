fcf3a1b84eb985ddb23061be6ed16d652ac51f5e
David M <PieKing1215@users.noreply.github.com>
2021-09-12 04:09:39 -0400

    Make build rerun on git HEAD changes

--- src/build.rs
+++ src/build.rs
@@ -7,6 +7,7 @@ fn main() {
     if let Ok(output) = Command::new("git").args(&["describe", "--always"]).output() {
         let git_hash = String::from_utf8(output.stdout).unwrap();
         println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+        println!("cargo:rustc-rerun-if-changed=.git/HEAD");
     }
 
     let now = Utc::now();
