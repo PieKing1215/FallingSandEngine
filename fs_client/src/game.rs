32e6051c2184aa8bc7264ad096e96e3b4ff70689
David M <PieKing1215@users.noreply.github.com>
2022-03-25 21:36:58 -0400

    Fix Pid fetching

--- fs_client/src/game.rs
+++ fs_client/src/game.rs
@@ -789,8 +789,8 @@ impl ClientGame {
                                 .as_str(),
                             );
 
-                            sys.refresh_process(Pid::from(std::process::id() as usize));
-                            if let Some(pc) = sys.process(Pid::from(std::process::id() as usize)) {
+                            sys.refresh_process(sysinfo::get_current_pid().unwrap());
+                            if let Some(pc) = sys.process(sysinfo::get_current_pid().unwrap()) {
                                 self.data.process_stats.cpu_usage =
                                     Some(pc.cpu_usage() / sys.processors().len() as f32);
                                 self.data.process_stats.memory = Some(pc.memory());
