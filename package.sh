8a7697884c5fed20d7873b53f20fe3e1ed7aa44d
David M <PieKing1215@users.noreply.github.com>
2022-03-26 15:26:46 -0400

    Create release-lto profile, don't strip exe in autobuild

--- package.sh
+++ package.sh
@@ -1,8 +1,8 @@
-if cargo build --release ; then
+if cargo build --profile release-lto ; then
     rm -rf package/
     mkdir package
     mkdir package/gamedir
-    cp target/release/fs_main.exe package/
+    cp target/release-lto/fs_main.exe package/
     cp -r gamedir/assets/ package/gamedir/assets/
     cargo lichking bundle --file package/dep_licenses.txt || true
 fi
