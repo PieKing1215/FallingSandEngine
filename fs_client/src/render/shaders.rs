10dd31d204ca788e07413c31033a39189c115775
David M <PieKing1215@users.noreply.github.com>
2022-04-21 23:11:38 -0400

    Fix a new clippy warning

--- fs_client/src/render/shaders.rs
+++ fs_client/src/render/shaders.rs
@@ -18,38 +18,38 @@ impl Shaders {
 
         Self {
             common: helper
-                .from_files(140, "data/shaders/common.vert", "data/shaders/common.frag")
+                .load_from_files(140, "data/shaders/common.vert", "data/shaders/common.frag")
                 .unwrap(),
             vertex_colors: helper
-                .from_files(
+                .load_from_files(
                     140,
                     "data/shaders/vert_colors.vert",
                     "data/shaders/vert_colors.frag",
                 )
                 .unwrap(),
             texture: helper
-                .from_files(
+                .load_from_files(
                     140,
                     "data/shaders/textured.vert",
                     "data/shaders/textured.frag",
                 )
                 .unwrap(),
             texture_array: helper
-                .from_files(
+                .load_from_files(
                     140,
                     "data/shaders/texture_array.vert",
                     "data/shaders/texture_array.frag",
                 )
                 .unwrap(),
             particle: helper
-                .from_files(
+                .load_from_files(
                     140,
                     "data/shaders/particles.vert",
                     "data/shaders/particles.frag",
                 )
                 .unwrap(),
             chunk: helper
-                .from_files(140, "data/shaders/chunk.vert", "data/shaders/chunk.frag")
+                .load_from_files(140, "data/shaders/chunk.vert", "data/shaders/chunk.frag")
                 .unwrap(),
         }
     }
@@ -61,7 +61,7 @@ struct Helper<'a> {
 }
 
 impl Helper<'_> {
-    fn from_files(
+    fn load_from_files(
         &self,
         version: u32,
         vert: &str,
