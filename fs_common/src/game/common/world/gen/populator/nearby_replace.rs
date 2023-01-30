8d65527e5e057bcfa9ef5b5fb9265ee9726d6d02
David M <PieKing1215@users.noreply.github.com>
2023-01-30 03:34:47 -0500

    Rework NearbyReplacePopulator interaction with neighbor chunks
    NearbyReplacePopulator no longer sets pixels in neighbors, but will now look in neighbors a bit for matching pixels

--- fs_common/src/game/common/world/gen/populator/nearby_replace.rs
+++ fs_common/src/game/common/world/gen/populator/nearby_replace.rs
@@ -18,17 +18,27 @@ impl Populator<1> for NearbyReplacePopulator {
         // otherwise this is basically just brute force
         // for each pixel that matches `searching_for`, scan around it and try to `replace`
 
-        let mut skip_y = [0; CHUNK_SIZE as usize];
+        const OVERSCAN: u16 = 2;
 
-        for y in 0..i32::from(CHUNK_SIZE) {
+        let mut skip_y = [0; CHUNK_SIZE as usize + OVERSCAN as usize * 2];
+
+        for y in -i32::from(OVERSCAN)..i32::from(CHUNK_SIZE) + i32::from(OVERSCAN) {
             let mut skip_x = 0;
-            for x in 0..i32::from(CHUNK_SIZE) {
+            for x in -i32::from(OVERSCAN)..i32::from(CHUNK_SIZE) + i32::from(OVERSCAN) {
                 let m = chunks.get(x, y).unwrap();
                 if (self.searching_for)(m) {
                     let range = i32::from(self.radius);
                     for dx in (-range + i32::from(skip_x))..=range {
-                        for dy in (-range + i32::from(skip_y[x as usize]))..=range {
+                        if x + dx < 0 || x + dx >= i32::from(CHUNK_SIZE) {
+                            continue;
+                        }
+                        for dy in
+                            (-range + i32::from(skip_y[x as usize + OVERSCAN as usize]))..=range
+                        {
                             if dx == 0 && dy == 0 {
+                                continue;
+                            }
+                            if y + dy < 0 || y + dy >= i32::from(CHUNK_SIZE) {
                                 continue;
                             }
                             let m2 = chunks.get(x + dx, y + dy).unwrap();
@@ -48,13 +58,13 @@ impl Populator<1> for NearbyReplacePopulator {
                     }
 
                     skip_x = self.radius * 2;
-                    skip_y[x as usize] = self.radius * 2;
+                    skip_y[x as usize + OVERSCAN as usize] = self.radius * 2;
                 } else if skip_x > 0 {
                     skip_x -= 1;
                 }
 
-                if skip_y[x as usize] > 0 {
-                    skip_y[x as usize] -= 1;
+                if skip_y[x as usize + OVERSCAN as usize] > 0 {
+                    skip_y[x as usize + OVERSCAN as usize] -= 1;
                 }
             }
         }
